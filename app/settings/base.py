883f132da45579b30d8bb3d5d2082454e98adc97
mohammed <mohammed.abdv@gmail.com>
2026-05-08 22:19:09 +0300

    update

--- app/settings/base.py
+++ app/settings/base.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': os.environ.get("POSTGRES_DB"),
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': 'postgres',
+        'HOST': os.environ.get("POSTGRES_HOST", "postgres"),
         'PORT': '5432',
     }
 }
